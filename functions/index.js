0957331428cc87f99d81e4f4a36dff3966880eae
Saurabh Singh <saur2702@gmail.com>
2019-09-01 19:50:38 +0530

    improved file hierarchy and moved functions accordingly

--- functions/index.js
+++ functions/index.js
@@ -1,108 +1,23 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const app = require('express')();
- 
-admin.initializeApp(); 
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDpa5Mw8DZZ0RWJd8AV7oYgxx0daNxNA-o",
-    authDomain: "socialape-12d50.firebaseapp.com",
-    databaseURL: "https://socialape-12d50.firebaseio.com",
-    projectId: "socialape-12d50",
-    storageBucket: "socialape-12d50.appspot.com",
-    messagingSenderId: "937857447745",
-    appId: "1:937857447745:web:ebc94287f9173b94"
-};
+const app = require('express')();
+
+const { getAllScreams } = require('./handlers/screams');
+const { signup, login } = require('./handlers/users');
+
 const firebase = require('firebase');
 firebase.initializeApp(firebaseConfig);
 
-const db = admin.firestore();
+//Scream routes
+app.get('/screams', getAllScreams);
+app.post('/scream', FBAuth, postOneScream);         //FBAuth is th emiddleware for authentication
 
-app.get('/screams', (req, res) => {
-    db
-    .collection('screams')
-    .orderBy('createdAt', 'desc')
-    .get()
-    .then(data => {
-        let screams = [];
-        data.forEach(doc => {
-            screams.push({
-                screamId: doc.id,
-                body: doc.data().body,
-                userHandle: doc.data().userHandle,
-                createdAt: doc.data().createdAt
-            });
-        });
-        return res.json(screams);
-    })
-    .catch(err => console.error(err));
-})
+// users route
+app.post('/signup', signup);
+app.post('/login', login);
 
-app.post('/scream', (req, res) => {
 
-    const newScream = {
-        body : req.body.body,
-        userHandle: req.body.userHandle,
-        createdAt: new Date().toISOString(),
-    };
-    db.collection('screams').add(newScream)
-    .then(doc => {
-        return res.json({message: `document ${doc.id} created successfully`});
-    })
-    .catch(err => {
-        res.status(500).json({ error: `something went wrong` });
-        console.error(err);
-    });
-});
 
-// Signup route
-app.post('/signup', (req, res) => {
-    const newUser = {
-        email: req.body.email,
-        password: req.body.password,
-        confirmPassword: req.body.confirmPassword,
-        handle: req.body.handle 
-    };
-
-    //Todo validate data
-    let token, userId;
-
-    db.doc(`/users/${newUser.handle}`).get()
-      .then(doc => {
-          if(doc.exists){
-              return res.status(400).json({ handle: 'this handle is already taken'});
-          }else{
-              return firebase
-              .auth()
-              .createUserWithEmailAndPassword(newUser.email, newUser.password);
-          }
-      })
-      .then (data => {
-          userId = data.user.uId;
-          return data.user.getIdToken();
-      })
-      .then((idtoken) => {
-          token = idtoken;
-          const userCredentials = {
-              handle: newUser.handle,
-              email: newUser.email,
-              createdAt: new Date().toISOString(),
-              userId
-          };
-          return db.doc(`/users/${newUser.handle}`).set(userCredentials);
-      })
-      .then(() => {
-          return screen.status(201).json({ token });
-      })
-      .catch(err => {
-          console.error(err);
-          if (err.code === "auth/email-already-in-use"){
-              return res.status(400).json({emial: 'Email already in use'});
-          }else{
-              return res.status(500).json({error : err.code});
-          }
-      }); 
-});
 
 // https://baseurl.com/api/     we are trying to get this prefix here - api
 exports.api = functions.region('asia-east2').https.onRequest(app); 
---
+++ functions/util/validators.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+const isEmpty = (string) => {
+    if (string.trim() === '') return true;
+    else return false;
+}
+
+const isEmail = (email) => {
+    const emailRegEx = /^(([^<>()\\[\]\\.,;:\s@"]+(\.[^<>()\\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (email.match(emailRegEx)) return true;
+    else return false;
+}
+
+
+exports.validateSignupData = (data) =>{ 
+    let errors = {};
+
+    if (isEmpty(data.email)) {
+        errors.email = 'Must not be empty'
+    } else if (!isEmail(data.email)) {
+        errors.email = 'Must be a valid email'
+    }
+    if (isEmpty(data.password)) errors.password = 'Must not be empty'
+    if (data.password !== data.confirmPassword) errors.confirmPassword = 'Passwords must match'
+    if (isEmpty(newUser.handle)) errors.handle = 'Must not be empty'
+
+    //To check if we have any erros above
+    return {
+        errors,
+        valid: Object.keys(errors).length === 0 ? true : false
+    }
+}
